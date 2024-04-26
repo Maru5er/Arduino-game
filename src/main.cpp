5deb89c81ec1b0087c28620087253e3f1d3821c6
Maru5er <74164821+Maru5er@users.noreply.github.com>
2024-04-25 18:11:18 -0700

    improved jump mechanics

--- src/main.cpp
+++ src/main.cpp
@@ -39,10 +39,16 @@ const unsigned char dino [] PROGMEM = {
 	0x01, 0x08, 0x00, 0x01, 0x08, 0x00, 0x01, 0x08, 0x00, 0x01, 0x8c, 0x00
 };
 
+const unsigned char ptero_sprite [] PROGMEM = {
+	0x04, 0x0f, 0xe0, 0x0e, 0x1e, 0x00, 0x1e, 0x1e, 0x00, 0x37, 0xff, 0xf0, 0xff, 0xff, 0x80, 0x3f, 
+	0xff, 0xe0, 0x00, 0xfe, 0x00
+};
+
 // Array of all bitmaps for convenience. (Total bytes used to store images in PROGMEM = 1040)
-const int epd_bitmap_allArray_LEN = 1;
-const unsigned char* epd_bitmap_allArray[1] = {
-	dino
+const int epd_bitmap_allArray_LEN = 2;
+const unsigned char* epd_bitmap_allArray[2] = {
+	dino,
+	ptero_sprite
 };
 
 const int BUTTON = 4;
@@ -56,15 +62,17 @@ const int PTERODACTYL_SPAWN_Y = SCORE_HEIGHT + 1;
 // cactus constants
 int cactus_x = SPAWN_X;
 int cactus_y = CACTUS_SPAWN_Y;
-int cactus_speed = 3;
+int cactus_speed = 4;
 int cactus_size = 12; // size of sprite
 int cactus_width = 5;
+const int MAX_CACTUS_HEIGHT = 12;
+const int NUM_CACTUS = 2;
 
 // pterodactyl constants
 int pterodactyl_x = SPAWN_X;
 int pterodactyl_y = PTERODACTYL_SPAWN_Y;
-int pterodactyl_size = 5;
-int pterodactyl_width = 12;
+int pterodactyl_height = 7;
+int pterodactyl_width = 20;
 int pterodactyl_speed = 1;
 
 int score = 0;
@@ -74,12 +82,15 @@ const int DINO_WIDTH = 20;
 const int DINO_HEIGHT = 20;
 const int DINO_X_HIT = 15;
 const int DINO_Y_HIT = 15;
+const int INITIAL_V = -11;
+int dino_jump_velocity = 0;
 int dino_x = 0; 
 int dino_y = SCREEN_HEIGHT - DINO_HEIGHT;
-int dino_speed = 4;
+int dino_speed = 6;
+const int DINO_GROUND = SCREEN_HEIGHT - DINO_HEIGHT;
 bool airborne = false;
 
-const int NUM_CACTUS = 2;
+
 
 class Cactus : public Rectangle {
   protected:
@@ -87,14 +98,16 @@ class Cactus : public Rectangle {
 
   public:
     Cactus(int x, int y, int width, int height) : Rectangle(x, y, width, height){}
+};
 
-    bool is_colliding() {
-      return _collide;
-    }
 
-    bool setHasPassedBird(bool collide) {
-      _collide = collide;
-    }
+class Ptero : public Rectangle {
+	public:
+	  Ptero(int x, int y, int width, int height) : Rectangle(x, y, width, height){}
+
+	  void drawPtero(Adafruit_SSD1306& display, int x, int y) {
+		display.drawBitmap(x, y, epd_bitmap_allArray[1], pterodactyl_width, pterodactyl_height, 1);
+	  }
 };
 
 Cactus cactuses[NUM_CACTUS] = {
@@ -102,6 +115,8 @@ Cactus cactuses[NUM_CACTUS] = {
 	Cactus(SPAWN_X, CACTUS_SPAWN_Y, cactus_width, cactus_size),
 
 };
+
+Ptero ptero = Ptero(0, PTERODACTYL_SPAWN_Y, pterodactyl_width, pterodactyl_height);
 
 bool is_colliding(int a_x, int a_y, int a_width, int a_height, int b_x, int b_y, int b_width, int b_height) {
 	bool OverX = (a_x < b_x + b_width) && (a_x + a_width > b_x);
@@ -121,12 +136,18 @@ void initEntities() {
 	int prev_x = min_cactus_spawn_x;
 	for (int i = 0; i < NUM_CACTUS; i++) {
 		int space = random(50, 90);
-		cactus_size = random(8, 20);
+		cactus_size = random(8, MAX_CACTUS_HEIGHT);
 		cactuses[i].setX(space + prev_x);
 		cactuses[i].setY(SCREEN_HEIGHT - cactus_size);
 		cactuses[i].setDimensions(cactus_width, cactus_size);
 		prev_x = cactuses[i].getX() + cactus_width;
 	}
+
+	// initialize pterodactyl
+	ptero.setX(0);
+	ptero.setY(PTERODACTYL_SPAWN_Y);
+	ptero.setDimensions(pterodactyl_width, pterodactyl_height);
+
 }
 
 bool button_pressed(int pin) {
@@ -139,12 +160,12 @@ bool button_pressed(int pin) {
 
 void gamePlayLoop() {
 	// move x of cactuses
+	int interval = random(50, 90);
 	for (int i = 0; i < NUM_CACTUS; i++) {
 		// check if cactus out of bound
 		// generate new one with random interval
 		randomSeed(analogRead(A0));
-		int interval = random(50, 90);
-		int new_height = random(8,20);
+		int new_height = random(8,MAX_CACTUS_HEIGHT);
 		int new_x = cactuses[i].getX() - cactus_speed;
 		if (new_x + cactus_width + interval < 0) {
 			// spawn new cactus from the right of the screen
@@ -165,27 +186,50 @@ void gamePlayLoop() {
 			cactuses[i].getX(), cactuses[i].getY(), 
 			cactuses[i].getWidth(), cactuses[i].getHeight())) {
 				score = 0;
+				tone(7, 500, 200);
 			}
 	}
 
+	// handle pterodactyl generation and movement
+	ptero.setX(ptero.getX() - pterodactyl_speed);
+	if (ptero.getX() + pterodactyl_width + interval  < 0) {
+		ptero.setX(SCREEN_WIDTH);
+		score++;
+	}
+	if (is_colliding(dino_x, dino_y, DINO_X_HIT, DINO_Y_HIT,
+					ptero.getX(), ptero.getY(), pterodactyl_width, pterodactyl_height)) {
+						score = 0;
+						tone(7, 500, 200);
+					}
+
 	// handle jump
+	// airborne only true when button is held
+	int gravity = 2;
+	if (button_pressed(BUTTON) && dino_y == DINO_GROUND) {
+		// apply jump force
+		dino_jump_velocity = INITIAL_V;
+		tone(7, 200, 100);
+	}
+	
+	if (dino_y > SCREEN_HEIGHT - DINO_HEIGHT) {
+		dino_jump_velocity = 0;
+	}
+
+	dino_y = min(dino_y + dino_jump_velocity, DINO_GROUND);
 	if (button_pressed(BUTTON)) {
-		airborne = true;
+		dino_jump_velocity += gravity;
+	} else {
+		dino_jump_velocity += gravity + 3;
 	}
-	if (airborne) {
-		dino_y -= dino_speed;
-		// max jump height
-		if (dino_y < SCREEN_HEIGHT - 20 - DINO_HEIGHT) {
-			dino_speed *= -1;
-		}
-		if (dino_y >= SCREEN_HEIGHT - 20) {
-			dino_speed *= -1;
-			airborne = false;
-		}
-	}
+	
 
 	// draw dino
 	display.drawBitmap(dino_x, dino_y, epd_bitmap_allArray[0], DINO_WIDTH, DINO_HEIGHT, 1);
+
+	
+	// draw ptero
+	ptero.drawPtero(display, ptero.getX(), ptero.getY());
+
 	
 }
 
@@ -206,7 +250,7 @@ void setup() {
   }
 
   display.display();
-  delay(2000);
+  delay(200);
   display.clearDisplay();
   // initialize entities
   initEntities();
@@ -226,8 +270,6 @@ void loop() {
 	display.setCursor(60, 0);
 	display.print("score: ");
 	display.print(score);
-	// draw pterodactyl
-	display.fillRect(pterodactyl_x, pterodactyl_y, pterodactyl_width, pterodactyl_size, 1);
 	
 
 	// gameplay loop
@@ -236,12 +278,7 @@ void loop() {
 	cactus_x -= 3;
 	pterodactyl_x -= 2;
 
-	// handle pterodactyl generation
-	if (pterodactyl_x < -pterodactyl_width) {
-		pterodactyl_x = SPAWN_X;
-		pterodactyl_y = random(SCORE_HEIGHT + 1,15); 
-		score++;
-	}
+	
 	
 }
 
